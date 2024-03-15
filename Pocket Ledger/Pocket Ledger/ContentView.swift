ff48ae8e8384495a5d56a989b3c8a59088b6c53a
JerryZhouSirui <1806430169@qq.com>
2024-03-14 21:58:00 -0400

    create xcodeproj

---
+++ Pocket Ledger/Pocket Ledger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket Ledger
+//
+//  Created by Jerry Zhou on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
