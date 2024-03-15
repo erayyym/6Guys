e6adbde8cc2a270448cd3d8448c89c184169ef9f
ssyang8 <shuishui@umich.edu>
2024-03-15 15:28:27 -0400

    entry-page-start

---
+++ PocketLedger/PocketLedger/module/scan/view/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  PocketLedger
+//
+//  Created by Chen Yang on 3/15/24.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController{
+    
+}
