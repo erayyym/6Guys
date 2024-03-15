153580e75bf06fc6b92e8fcf3a87a3f2b621dd5b
JerryZhouSirui <1806430169@qq.com>
2024-03-14 23:08:44 -0400

    change view file names

--- Pocket Ledger/Pocket Ledger/categorization /ContentView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ContentView.swift
-//  Pocket Ledger
-//
-//  Created by myr on 2024/3/14.
-//
-
-import Foundation
---
+++ Pocket Ledger/Pocket Ledger/gptAnalysis/AnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalysisView.swift
+//  Pocket Ledger
+//
+//  Created by Jerry Zhou on 3/14/24.
+//
+
+import SwiftUI
+
+struct AnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalysisView()
+}
