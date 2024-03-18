1197b8206b86a967b51c8123058cc9efd152dd0e
ssyang8 <shuishui@umich.edu>
2024-03-17 21:04:18 -0400

    scanview select pics from album

--- PocketLedger/PocketLedger/ContentView.swift
+++ PocketLedger/PocketLedger/ContentView.swift
@@ -2,23 +2,54 @@
 //  ContentView.swift
 //  PocketLedger
 //
-//  Created by Jerry Zhou on 3/14/24.
+//  Created by Chen Yang on 3/17/24.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var image: UIImage?
+    @State private var isImagePickerPresented = false
+    @State private var sourceType: UIImagePickerController.SourceType?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 250)
+            } else {
+                Text("No image selected")
+                    .padding() // Optional, for some spacing around the text if needed.
+            }
+
+            
+            Button("Scan") {
+                self.sourceType = nil
+                isImagePickerPresented.toggle()
+            }
+            .foregroundColor(.blue)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.blue, lineWidth: 1)
+            )
+            .sheet(isPresented: $isImagePickerPresented) {
+                if let sourceType = self.sourceType {
+                    ImagePicker(image: $image, sourceType: sourceType)
+                } else {
+                    ImagePicker(image: $image, sourceType: .photoLibrary)
+                }
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ PocketLedger/PocketLedger/ImagePicker.swift
@@ -0,0 +1,46 @@
+//
+//  ImagePicker.swift
+//  PocketLedger
+//
+//  Created by Chen Yang on 3/16/24.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    var sourceType: UIImagePickerController.SourceType
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                self.parent.image = image
+            }
+            picker.dismiss(animated: true)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+}
+
