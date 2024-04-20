b5ac4fca8541ed996f824c42da2223942ff601ed
ssyang8 <143631453+ssyang8@users.noreply.github.com>
2024-04-19 23:42:27 -0400

    Update CGImagePropertyOrientation+UIImageOrientation.swift

--- PocketLedger/PocketLedger/CGImagePropertyOrientation+UIImageOrientation.swift
+++ PocketLedger/PocketLedger/CGImagePropertyOrientation+UIImageOrientation.swift
@@ -9,10 +9,6 @@ import UIKit
 import ImageIO
 
 extension CGImagePropertyOrientation {
-    /// Converts an image orientation to a Core Graphics image property orientation.
-    /// - Parameter orientation: A `UIImage.Orientation` instance.
-    ///
-    /// The two orientation types use different raw values.
     init(_ orientation: UIImage.Orientation) {
         switch orientation {
             case .up: self = .up
